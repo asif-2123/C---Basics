325226940114119a2f4ad9365bc5402c7d527bc5
Md Asif <asif212327@gmail.com>
2026-01-19 00:54:38 +0530

    Add files via upload

---
+++ C - Programming/functioncall.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void table (int n );
+int main (){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    table(n);
+    return 0;
+}
+void table (int n) {
+    for (int i = 1 ; i <= 10 ; i ++){
+        printf("%d\n", n*i);
+    }
+}
